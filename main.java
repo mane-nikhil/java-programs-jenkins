f483043f65fd2578323f2b9b6da64689cc2b6ed8
mane-nikhil <nm3970902@gmail.com>
2025-02-19 10:27:32 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+class main {
+  public static void main(String[] args) {
+    System.out.println("hello world");
+    System.out.println("output from java program");
+    System.out.println(new Date());
+
+  }
+}
